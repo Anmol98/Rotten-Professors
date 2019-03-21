f150f47afe12e7a106996311825ed9316ff079e3
Anmol98 <gup.anmol98@gmail.com>
2019-03-21 15:36:42 +0530

    Solved media problems, second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'rotprof\static\media')  #this defines the path to media files
+MEDIA_URL = '/media/'
 print(MEDIA_ROOT)
--- rotprof/urls.py
+++ rotprof/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.branch_list, name='branch_list'),
     path('post/<str:pk>/', views.prof_list_branch, name='prof_list_branch')
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
